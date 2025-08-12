6d8327b15f17ede0d8e2e72d37d4a8d1b6f1f3ad
sihwan-1 <rlatlghks0917@gmail.com>
2025-08-12 13:37:04 +0900

    header 작업중

---
+++ script.js
@@ -0,0 +1,50 @@
+$('.start-popup').addClass('close'); // 임시
+
+// start popup 스크립트
+function Popup_close() {
+  $('.popup-close > p').click(function () {
+    $('.start-popup').addClass('close');
+  });
+}
+Popup_close();
+
+
+
+$(window).on('scroll', function () {
+  let scrollTop = $(this).scrollTop();
+
+  if (scrollTop > 20) {
+    $('header').addClass('on');
+  } else {
+    $('header').removeClass('on');
+  }
+});
+
+
+function Coupon_close() {
+  $('.coupon-close').click(function () {
+    $('.coupon').addClass('close');
+  });
+}
+Coupon_close();
+
+function M_menu() {
+  $('.side-menu > nav > ul > li').click(function() {
+    let $this = $(this);
+    
+    $this.siblings('.hover').removeClass('hover');
+    
+    if ( $this.hasClass('hover') ) {
+      $this.removeClass('hover');
+    }
+    else {
+      $this.addClass('hover');
+    }
+  });
+  
+  $('.side-menu > nav > ul > li > ul').click(function() {
+    return false;
+  });
+}
+
+M_menu();

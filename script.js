635be66bb97e41ed56ad7eed16b935c6461d7c72
sihwan-1 <rlatlghks0917@gmail.com>
2025-08-13 13:43:59 +0900

    섹션 7완성

--- script.js
+++ script.js
@@ -52,14 +52,60 @@ M_menu();
 function Side_menu() {
   $('.m-menu').click(function () {
     $('.side-menu').addClass('active');
+    $('.bg').addClass('blur');
   });
 
-  $('.close-btn').click(function () {
+  $('.close-btn').click(function (e) {
+    e.stopPropagation();
     $('.side-menu').removeClass('active');
+    $('.bg').removeClass('blur');
   });
 }
 Side_menu();
 
-$('.close-btn').click(function () {
-  console.log("ddd");
+$(window).on('scroll', function () {
+  let scrollTop = $(this).scrollTop();
+
+  if (scrollTop > 20) {
+    $('header').addClass('on');
+  } else {
+    $('header').removeClass('on');
+  }
 });
+
+function Swiper_2_m() {
+  var swiper = new Swiper(".swiper-2-m", {
+    slidesPerView: 1.5,
+    spaceBetween: 14,
+  });
+}
+Swiper_2_m();
+
+function Swiper_3_m() {
+  var swiper = new Swiper(".swiper-3-m", {
+    slidesPerView: 1,
+    navigation: {
+      nextEl: ".swiper-box-3-m .swiper-button-next",
+      prevEl: ".swiper-box-3-m .swiper-button-prev",
+    },
+  });
+}
+Swiper_3_m();
+
+function Swiper_4_m() {
+  var swiper = new Swiper(".swiper-4-m", {
+    slidesPerView: 1.5,       // 양옆이 보이도록
+    spaceBetween: 20,
+    centeredSlides: true,     // 가운데 정렬
+    loop: true,
+  });
+}
+Swiper_4_m();
+
+function Swiper_5_m() {
+  var swiper = new Swiper(".swiper-5-m", {
+    slidesPerView: 1.5,
+    spaceBetween: 14,
+  });
+}
+Swiper_5_m();
